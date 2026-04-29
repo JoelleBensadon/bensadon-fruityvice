86b2c092c6d583019b4e65dd6f8cb7a9dec8943c
joellebensadon <jbens2004@gmail.com>
2026-04-28 23:22:18 -0400

    fix unsplash test and checkstyle

--- src/test/java/bensadon/fruityvice/unsplash/UnsplashServiceTest.java
+++ src/test/java/bensadon/fruityvice/unsplash/UnsplashServiceTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UnsplashServiceTest {
 
     @Test
-    void search() {
+    void photosStructure() {
 
         // Given
         ApiKey apiKey = new ApiKey();
