20fe97c272da022800521dd386b4062cb07b5bc7
JoelleBensadon <jbens2004@gmail.com>
2026-04-28 23:16:20 -0400

    Update src/test/java/bensadon/fruityvice/unsplash/UnsplashServiceTest.java
    Co-authored-by: Andrew Schwimmer <andrew.schwimmer@gmail.com>

--- src/test/java/bensadon/fruityvice/unsplash/UnsplashServiceTest.java
+++ src/test/java/bensadon/fruityvice/unsplash/UnsplashServiceTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class UnsplashServiceTest {
 
     @Test
-    void photosStructure() {
+    void search() {
 
         // Given
         ApiKey apiKey = new ApiKey();
